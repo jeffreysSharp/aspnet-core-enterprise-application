d54fd6ba559a2bf372d4583637f3af6126c9451a
jeffreysSharp <jefferson_qi3@hotmail.com>
2024-09-21 20:12:11 -0300

    Configuração do Identity / Registro e Login de Usuário /  Documentação da API e Testes

---
+++ JeffStoreEnterprise/src/services/JSE.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using JSE.Identidade.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JSE.Identidade.API.Controllers
+{
+    [ApiController]
+    [Route("api/identidade")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(UsuarioRegistroViewModel usuarioRegistroViewModel)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = usuarioRegistroViewModel.Email,
+                Email = usuarioRegistroViewModel.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, usuarioRegistroViewModel.Senha);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Ok();
+            }
+
+
+            return BadRequest();
+            ;
+        }
+
+        [HttpPost("autenticar")]
+        public async Task<ActionResult> Login(UsuarioLoginViewModel usuarioLoginViewModel)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result = await _signInManager.PasswordSignInAsync(usuarioLoginViewModel.Email, usuarioLoginViewModel.Senha, false, true);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
