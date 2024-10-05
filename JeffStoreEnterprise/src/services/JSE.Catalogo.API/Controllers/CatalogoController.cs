4566c5689559fcc2a843a3ac9e23ac6e0146e44f
jeffreysSharp <jefferson_qi3@hotmail.com>
2024-10-05 14:14:38 -0300

    Criar Comportamentos básicos API Catalogo
    #60 - Criar Comportamentos básicos API Catalogo

---
+++ JeffStoreEnterprise/src/services/JSE.Catalogo.API/Controllers/CatalogoController.cs
@@ -0,0 +1,30 @@
+﻿using JSE.Catalogo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JSE.Catalogo.API.Controllers
+{
+    [ApiController]
+    public class CatalogoController : Controller
+    {
+
+        private readonly IProdutoRepository _produtoRepository;
+
+        public CatalogoController(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        [HttpGet("catalogo/produtos")]
+        public async Task<IEnumerable<Produto>> Index()
+        {
+            return await _produtoRepository.ObterTodos();
+        }
+
+        [HttpGet("catalogo/produtos/{id}")]
+        public async Task<Produto> Produtodetalhe(Guid id)
+        {
+            return await _produtoRepository.ObterPorId(id);
+        }
+
+    }
+}
