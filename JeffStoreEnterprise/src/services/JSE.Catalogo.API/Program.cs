54887ef3352a991f4b1e2abedc01e858e640ac49
jeffreysSharp <jefferson_qi3@hotmail.com>
2024-10-05 14:14:38 -0300

    Criar Comportamentos básicos API Catalogo
    #60 - Criar Comportamentos básicos API Catalogo

--- JeffStoreEnterprise/src/services/JSE.Catalogo.API/Program.cs
+++ JeffStoreEnterprise/src/services/JSE.Catalogo.API/Program.cs
@@ -2,6 +2,7 @@ using JSE.Catalogo.API.Data;
 using JSE.Catalogo.API.Data.Repository;
 using JSE.Catalogo.API.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args); 
 
@@ -16,12 +17,34 @@ builder.Services.AddDbContext<CatalogoContext>(x => x.UseSqlServer(connectionStr
 
 var app = builder.Build();
 
+builder.Configuration
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", false, false)
+        .AddJsonFile($@"appsettings.{builder.Environment.EnvironmentName}.json", false, false)
+        .AddCommandLine(args)
+        .AddEnvironmentVariables()
+        .AddUserSecrets(typeof(Program).Assembly).Build();
+
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//builder.Services.AddSwaggerGen(c =>
+//    c.SwaggerDoc("v1", new OpenApiInfo
+//    {
+//        Title = "JeffStore Enterprise Identity API",
+//        Description = "Esta é a API da JeffStore Enterprise Applications",
+//        Contact = new OpenApiContact() { Name = "Jeferson Almeida", Email = "jefferson_qi3@hotmail.com" },
+//        License = new OpenApiLicense()
+//        {
+//            Name = "MIT",
+//            Url = new Uri("https://opensource.org/licenses/MIT")
+//        }
+//    })
+//);
 
 app.UseHttpsRedirection();
 
