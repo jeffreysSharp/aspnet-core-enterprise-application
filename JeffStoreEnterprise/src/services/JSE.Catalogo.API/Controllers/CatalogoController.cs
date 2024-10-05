0fe808752afeb50902ede2256715671f162757a9
jeffreysSharp <jefferson_qi3@hotmail.com>
2024-10-05 17:26:49 -0300

    Criar Validação de acesso baseado em claims
    #62 - Criar Validação de acesso baseado em claims

--- JeffStoreEnterprise/src/services/JSE.Catalogo.API/Controllers/CatalogoController.cs
+++ JeffStoreEnterprise/src/services/JSE.Catalogo.API/Controllers/CatalogoController.cs
@@ -1,9 +1,12 @@
-﻿using JSE.Catalogo.API.Models;
+﻿using JSE.Catalogo.API.Extensions;
+using JSE.Catalogo.API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JSE.Catalogo.API.Controllers
 {
     [ApiController]
+    [Authorize]
     public class CatalogoController : Controller
     {
 
@@ -14,12 +17,14 @@ namespace JSE.Catalogo.API.Controllers
             _produtoRepository = produtoRepository;
         }
 
+        [AllowAnonymous]
         [HttpGet("catalogo/produtos")]
         public async Task<IEnumerable<Produto>> Index()
         {
             return await _produtoRepository.ObterTodos();
         }
 
+        [ClaimsAuthorize("Catalogo", "Ler")]
         [HttpGet("catalogo/produtos/{id}")]
         public async Task<Produto> Produtodetalhe(Guid id)
         {
