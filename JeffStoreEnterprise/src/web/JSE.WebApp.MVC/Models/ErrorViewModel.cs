01edb1926d9bb5070862ed896d0c3877bd6bcfa6
jeffreysSharp <jefferson_qi3@hotmail.com>
2024-10-06 03:40:53 -0300

    Criar Circuit Breaker com Polly
    #67 - Criar Circuit Breaker com Polly

--- JeffStoreEnterprise/src/web/JSE.WebApp.MVC/Models/ErrorViewModel.cs
+++ JeffStoreEnterprise/src/web/JSE.WebApp.MVC/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace JSE.WebApp.MVC.Models
 {
     public class ErrorViewModel
     {
-        public int ErrorCode { get; set; }
+        public int ErroCode { get; set; }
         public string Titulo { get; set; }
         public string Mensagem { get; set; }
     }
