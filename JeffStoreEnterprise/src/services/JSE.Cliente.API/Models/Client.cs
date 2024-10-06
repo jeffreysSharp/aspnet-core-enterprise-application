bb5ca879e7add09f0547c26160f771ce8ec20060
jeffreysSharp <jefferson_qi3@hotmail.com>
2024-10-06 13:19:14 -0300

    Definir as entidades
    #90 - Definir as entidades

---
+++ JeffStoreEnterprise/src/services/JSE.Cliente.API/Models/Client.cs
@@ -0,0 +1,32 @@
+﻿using JSE.Core.DomainObjects;
+
+namespace JSE.Cliente.API.Models
+{
+    public class Client : Entity, IAggregateRoot
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Surname {  get; private set; }
+        public Guid GenderId { get; set; }
+        public string Email { get; private set; }
+        public string Phone { get; private set; }
+        public DateTime BirthdayDate { get; private set; }
+        public string Document { get; private set; }
+        public bool IsRemoved { get; private set; }
+        public List<ClientAddress> Addresses { get; private set; }
+        public List<ClientPurchaseHistory> PurchaseHistories { get; private set; }
+
+        public Client()  { }
+
+        public Client(string firstName, string lastName, Guid genderId, string email, string phone, DateTime birthdayDate, string document)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+            BirthdayDate = birthdayDate;
+            Document = document;
+            IsRemoved = false;
+        }
+    }
+}
