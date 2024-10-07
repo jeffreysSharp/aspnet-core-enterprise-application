a653812fc1d5c092bf16fae23e29ea89287ee003
jeffreysSharp <jefferson_qi3@hotmail.com>
2024-10-07 11:52:26 -0300

    Correção Erro ao carregar Projeto ClientAPI
    Correção Erro ao carregar Projeto ClientAPI

---
+++ JeffStoreEnterprise/src/services/JSE.Client.API/Models/ClientPurchaseHistory.cs
@@ -0,0 +1,11 @@
+﻿using JSE.Core.DomainObjects;
+
+namespace JSE.Cliente.API.Models
+{
+    public class ClientPurchaseHistory : Entity
+    {
+        public Guid OrderId { get; private set; }
+        public Guid ClientId { get; private set; }
+        public Client Client { get; private set; }        
+    }
+}
