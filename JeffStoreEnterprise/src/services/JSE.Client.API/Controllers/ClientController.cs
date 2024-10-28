1d10a6270a141b66dace65e548638e4170652b62
Jeferson Almeida <jefferson_qi3@hotmail.com>
2024-10-28 10:48:22 -0300

    Implementar Request Response Pattern

--- JeffStoreEnterprise/src/services/JSE.Client.API/Controllers/ClientController.cs
+++ JeffStoreEnterprise/src/services/JSE.Client.API/Controllers/ClientController.cs
@@ -13,14 +13,14 @@ namespace JSE.Client.API.Controllers
             _mediatorHandler = mediatorHandler;
         }
 
-        [HttpGet("clientes")]
-        public async Task<IActionResult> Index()
-        {
-           var result = await _mediatorHandler.SendCommand(new RegisterClientCommand(Guid.NewGuid(), "Jeferson", "Almeida",
-                "Jé", Guid.Parse("B91AFC01-B0B7-4B5E-98BD-885F2BE32780"), "jefferson_qi3@hotmail.com", "11997541210", 
-                DateTime.Now, "29492873877"));
+        //[HttpGet("clientes")]
+        //public async Task<IActionResult> Index()
+        //{
+        //   var result = await _mediatorHandler.SendCommand(new RegisterClientCommand(Guid.NewGuid(), "Jeferson", "Almeida",
+        //        "Jé", "B91AFC01-B0B7-4B5E-98BD-885F2BE32780", "jefferson_qi3@hotmail.com", "11997541210", 
+        //        DateTime.Now.ToString(), "29492873877"));
 
-            return CustomResponse(result);  
-        }
+        //    return CustomResponse(result);  
+        //}
     }
 }
---
+++ JeffStoreEnterprise/src/services/JSE.Client.API/Services/RegistroClienteIntegrationHandler.cs
@@ -0,0 +1,46 @@
+﻿
+using EasyNetQ;
+using FluentValidation.Results;
+using JSE.Client.API.Application.Commands;
+using JSE.Core.Integration;
+using JSE.Core.Mediator;
+
+namespace JSE.Client.API.Services
+{
+    public class RegistroClienteIntegrationHandler : BackgroundService
+    {
+        private IBus _bus;
+        private readonly IServiceProvider _serviceProvider;
+
+        public RegistroClienteIntegrationHandler(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _bus = RabbitHutch.CreateBus("host=localhost:5672");
+
+            _bus.Rpc.RespondAsync<UsuarioRegistradoIntegrationEvent, ResponseMessage>(async request =>
+                new ResponseMessage(await RegistrarCliente(request)));
+
+            return Task.CompletedTask;
+        }
+
+        private async Task<ValidationResult> RegistrarCliente(UsuarioRegistradoIntegrationEvent message)
+        {
+            var clienteCommand = new RegisterClientCommand(message.Id, message.FirstName, message.LastName, message.Surname, message.GenderId,
+                message.Email, message.Phone, message.BirthdayDate, message.DocumentNumber);
+
+            ValidationResult sucesso;
+
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var mediator = scope.ServiceProvider.GetRequiredService<IMediatorHandler>();
+                sucesso = await mediator.SendCommand(clienteCommand);
+            }
+            
+            return sucesso;
+        }
+    }
+}
