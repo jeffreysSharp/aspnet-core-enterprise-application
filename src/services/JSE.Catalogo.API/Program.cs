24659e8a47fb11a0d675dee5f8b5fe3f060a0d7e
Jeferson Almeida <jefferson_qi3@hotmail.com>
2024-11-01 08:13:17 -0300

    Refatoração Catalogo API

--- src/services/JSE.Catalogo.API/Program.cs
+++ src/services/JSE.Catalogo.API/Program.cs
@@ -1,93 +1,10 @@
-using JSE.Catalogo.API.Data;
-using JSE.Catalogo.API.Data.Repository;
-using JSE.Catalogo.API.Models;
+using JSE.Catalogo.API.Configuration;
 using JSE.WebAPI.Core.IdentityConfiguration;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
-builder.Services.AddControllers();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<CatalogoContext>(x => x.UseSqlServer(connectionString));
-
-// JWT
-
-var appSettingsSection = builder.Configuration.GetSection("AppSettings");
-builder.Services.Configure<AppSettings>(appSettingsSection);
-
-var appSettings = appSettingsSection.Get<AppSettings>();
-var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
-}).AddJwtBearer(bearerOptions =>
-{
-    bearerOptions.RequireHttpsMetadata = true;
-    bearerOptions.SaveToken = true;
-    bearerOptions.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidAudience = appSettings.ValidOn,
-        ValidIssuer = appSettings.Issuer
-    };
-});
-
-builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
-builder.Services.AddScoped<CatalogoContext>();
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo()
-    {
-        Title = "JeffStore Enterprise Catálogo API",
-        Description = "Esta é a API da JeffStore Enterprise Applications",
-        Contact = new OpenApiContact() { Name = "Jeferson Almeida", Email = "jefferson_qi3@hotmail.com" },
-        License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
-    });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "Insira o token JWT desta maneira: Bearer {seu token}",
-        Name = "Authorization",
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
-});
-
-
-var app = builder.Build();
-
-builder.Configuration
+configuration
         .SetBasePath(Directory.GetCurrentDirectory())
         .AddJsonFile("appsettings.json", false, false)
         .AddJsonFile($@"appsettings.{builder.Environment.EnvironmentName}.json", false, false)
@@ -95,16 +12,16 @@ builder.Configuration
         .AddEnvironmentVariables()
         .AddUserSecrets(typeof(Program).Assembly).Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+builder.Services.AddApiConfiguration(configuration);
+builder.Services.AddJwtConfiguration(configuration);
+builder.Services.AddSwaggerConfiguration();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.RegisterServices();
 
-app.UseHttpsRedirection();
+var app = builder.Build();
+var environment = app.Environment;
 
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseSwaggerConfiguration();
+app.UseApiConfiguration(environment);
 
 app.Run();
