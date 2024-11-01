e456e17f0fa0d6fabf4701d37f57304cb6d3f8cb
Jeferson Almeida <jefferson_qi3@hotmail.com>
2024-11-01 08:49:25 -0300

    Refatoração do ClientAPI

---
+++ src/services/JSE.Client.API/Configuration/MessageBusConfig.cs
@@ -0,0 +1,16 @@
+﻿using JSE.Client.API.Services;
+using JSE.Core.Utils;
+using JSE.MessageBus;
+
+namespace JSE.Client.API.Configuration
+{
+    public static class MessageBusConfig
+    {
+        public static void AddMessageBusConfiguration(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"))
+                .AddHostedService<RegistroClienteIntegrationHandler>();
+        }
+    }
+}
