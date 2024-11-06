e48d56cdd9bd0e18eca6f9d304664a5582c52880
Jeferson Almeida <jefferson_qi3@hotmail.com>
2024-11-06 13:58:34 -0300

    #172- Definir a arquitetura da API de Pedidos feature/sp6/#172

---
+++ src/services/JSE.Pedido.Infra/Data/PedidosContext.cs
@@ -0,0 +1,22 @@
+﻿using JSE.Core.Data;
+using JSE.Core.Mediator;
+using Microsoft.EntityFrameworkCore;
+
+namespace JSE.Pedidos.Infra.Data
+{
+    public class PedidosContext : DbContext, IUnitOfWork
+    {
+        private readonly IMediatorHandler _mediatorHandler;
+
+        public PedidosContext(DbContextOptions<PedidosContext> options, IMediatorHandler mediatorHandler)
+            : base(options)
+        {
+            _mediatorHandler = mediatorHandler;
+        }
+
+        public Task<bool> Commit()
+        {
+            throw new NotImplementedException();
+        }
+    } 
+}
