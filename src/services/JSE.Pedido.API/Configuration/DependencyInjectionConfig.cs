1d313ad1affd1a0922a3ef0cfa085942da8d8d03
Jeferson Almeida <jefferson_qi3@hotmail.com>
2024-11-06 13:58:34 -0300

    #172- Definir a arquitetura da API de Pedidos feature/sp6/#172

---
+++ src/services/JSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,25 @@
+﻿using JSE.WebAPI.Core.User;
+
+namespace JSE.Pedidos.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            // API
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();
+            
+            // Commands
+
+            // Events
+
+
+            // Application
+
+
+            // Data
+
+        }
+    }
+}
---
+++ src/services/JSE.Pedido.API/Configuration/MessageBusConfig.cs
@@ -0,0 +1,14 @@
+﻿using JSE.Core.Utils;
+using JSE.MessageBus;
+
+namespace NSE.Pedidos.API.Configuration
+{
+    public static class MessageBusConfig
+    {
+        public static void AddMessageBusConfiguration(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"));
+        }
+    }
+}
