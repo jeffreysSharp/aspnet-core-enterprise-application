d8bf608af3a5c0da8c2eea7569ba0be77c19c09b
Jeferson Almeida <jefferson_qi3@hotmail.com>
2024-11-06 12:55:42 -0300

    Conectar o BFF na WebApp

--- src/web/JSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/JSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -30,7 +30,7 @@ namespace JSE.WebApp.MVC.Configuration
                 .AddTransientHttpErrorPolicy(
                     p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
-            services.AddHttpClient<ICarrinhoService, CarrinhoService>()
+            services.AddHttpClient<IComprasBffService, ComprasBffServiceService>()
                 .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
                 .AddPolicyHandler(PollyExtensions.EsperarTentar())
                 .AddTransientHttpErrorPolicy(
---
+++ src/web/JSE.WebApp.MVC/Services/IComprasBffService.cs
@@ -0,0 +1,14 @@
+﻿using JSE.Core.Communication;
+using JSE.WebApp.MVC.Models;
+
+namespace JSE.WebApp.MVC.Services
+{
+    public interface IComprasBffService
+    {
+        Task<CarrinhoViewModel> ObterCarrinho();
+        Task<int> ObterQuantidadeCarrinho();
+        Task<ResponseResult> AdicionarItemCarrinho(ItemCarrinhoViewModel produto);
+        Task<ResponseResult> AtualizarItemCarrinho(Guid produtoId, ItemCarrinhoViewModel produto);
+        Task<ResponseResult> RemoverItemCarrinho(Guid produtoId);
+    }
+}
