a71265b8337659daa7a73acd34aa4f72b9b2031d
Jeferson Almeida <jefferson_qi3@hotmail.com>
2024-11-06 19:03:57 -0300

    #176 - Aplicar o voucher na API de Carrinho

--- src/services/JSE.Carrinho.API/Migrations/20241106215248_Voucher.Designer.cs
+++ src/services/JSE.Carrinho.API/Migrations/20241106215248_Voucher.Designer.cs
@@ -4,6 +4,7 @@ using JSE.Carrinho.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JSE.Carrinho.API.Migrations
 {
     [DbContext(typeof(CarrinhoContext))]
-    partial class CarrinhoContextModelSnapshot : ModelSnapshot
+    [Migration("20241106215248_Voucher")]
+    partial class Voucher
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
