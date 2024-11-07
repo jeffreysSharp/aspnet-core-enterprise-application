e2c5c40d40a594ee629c6496f14dde5eeeebc0a1
Jeferson Almeida <jefferson_qi3@hotmail.com>
2024-11-07 17:03:02 -0300

    Correção de erro na secret da Cliente.API

---
+++ src/services/JSE.Pedido.API/Application/Events/PedidoEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using JSE.Core.Messages.Integration;
+using JSE.MessageBus;
+using MediatR;
+
+namespace JSE.Pedido_API.Application.Events
+{
+    public class PedidoEventHandler : INotificationHandler<PedidoRealizadoEvent>
+    {
+        private readonly IMessageBus _bus;
+
+        public PedidoEventHandler(IMessageBus bus)
+        {
+            _bus = bus;
+        }
+
+        public async Task Handle(PedidoRealizadoEvent message, CancellationToken cancellationToken)
+        {
+            await _bus.PublishAsync(new PedidoRealizadoIntegrationEvent(message.ClienteId));
+        }
+    }
+}
