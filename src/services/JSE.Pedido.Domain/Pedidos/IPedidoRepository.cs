896eaca816d3e924749987eaba5f1a88365e9469
Jeferson Almeida <jefferson_qi3@hotmail.com>
2024-11-07 10:20:44 -0300

    #178 - Modelar o pedido no domínio

---
+++ src/services/JSE.Pedido.Domain/Pedidos/IPedidoRepository.cs
@@ -0,0 +1,17 @@
+﻿using JSE.Core.Data;
+using System.Data.Common;
+
+namespace JSE.Pedidos.Domain.Pedidos
+{
+    public interface IPedidoRepository : IRepository<Pedido>
+    {
+        Task<Pedido> ObterPorId(Guid id);
+        Task<IEnumerable<Pedido>> ObterListaPorClienteId(Guid clienteId);
+        void Adicionar(Pedido pedido);
+        void Atualizar(Pedido pedido);
+
+        // Pedido Item        
+        Task<PedidoItem> ObterItemPorId(Guid id);
+        Task<PedidoItem> ObterItemPorPedido(Guid pedidoId, Guid produtoId);
+    }
+}
