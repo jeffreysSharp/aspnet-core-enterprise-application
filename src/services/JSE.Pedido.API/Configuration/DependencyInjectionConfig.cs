010f08cd198a16e0fb111b2173e63ed9029c508d
Jeferson Almeida <jefferson_qi3@hotmail.com>
2024-11-07 17:44:39 -0300

    #185 - Finalizando a API de Pedidos

--- src/services/JSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/JSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
@@ -1,9 +1,16 @@
-﻿using JSE.Core.Mediator;
+﻿using FluentValidation.Results;
+using JSE.Core.Mediator;
+using JSE.Pedido_API.Application.Events;
 using JSE.Pedido_API.Application.Queries;
+using JSE.Pedidos.API.Application.Commands;
+using JSE.Pedidos.API.Application.Queries;
 using JSE.Pedidos.Domain;
+using JSE.Pedidos.Domain.Pedidos;
 using JSE.Pedidos.Infra.Data;
 using JSE.Pedidos.Infra.Data.Repository;
 using JSE.WebAPI.Core.User;
+using MediatR;
+using NSE.Pedidos.API.Application.Commands;
 using System.Reflection;
 
 namespace JSE.Pedidos.API.Configuration
@@ -17,16 +24,19 @@ namespace JSE.Pedidos.API.Configuration
             services.AddScoped<IAspNetUser, AspNetUser>();
 
             // Commands
+            services.AddScoped<IRequestHandler<AdicionarPedidoCommand, ValidationResult>, PedidoCommandHandler>();
 
             // Events
-
+            services.AddScoped<INotificationHandler<PedidoRealizadoEvent>, PedidoEventHandler>();
 
             // Application
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
             services.AddScoped<IMediatorHandler, MediatorHandler>();
             services.AddScoped<IVoucherQueries, VoucherQueries>();
+            services.AddScoped<IPedidoQueries, PedidoQueries>();
 
             // Data
+            services.AddScoped<IPedidoRepository, PedidoRepository>();
             services.AddScoped<IVoucherRepository, VoucherRepository>();
             services.AddScoped<PedidosContext>();
 
