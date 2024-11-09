ad820ed457d630eb0147af46ee080a950addfc04
Jeferson Almeida <jefferson_qi3@hotmail.com>
2024-11-09 08:43:00 -0300

    #186 - Cadastro e consulta de endereço do cliente

---
+++ src/web/JSE.WebApp.MVC/Services/IClienteService.cs
@@ -0,0 +1,11 @@
+﻿using JSE.Core.Communication;
+using JSE.WebApp.MVC.Models;
+
+namespace JSE.WebApp.MVC.Services
+{
+    public interface IClienteService
+    {
+        Task<EnderecoViewModel> ObterEndereco();
+        Task<ResponseResult> AdicionarEndereco(EnderecoViewModel endereco);
+    }
+}
