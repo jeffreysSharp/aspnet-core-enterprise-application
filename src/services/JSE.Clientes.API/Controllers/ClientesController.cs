4c856df3f5bce497204af2a2fa98c6f67886078b
Jeferson Almeida <jefferson_qi3@hotmail.com>
2024-11-09 08:43:00 -0300

    #186 - Cadastro e consulta de endereço do cliente

---
+++ src/services/JSE.Clientes.API/Controllers/ClientesController.cs
@@ -0,0 +1,38 @@
+﻿using JSE.Clientes.API.Application.Commands;
+using JSE.Clientes.API.Models;
+using JSE.Core.Mediator;
+using JSE.WebAPI.Core.Controllers;
+using JSE.WebAPI.Core.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JSE.Clientes.API.Controllers
+{
+    public class ClientesController : MainController
+    {
+        private readonly IClienteRepository _clienteRepository;
+        private readonly IMediatorHandler _mediator;
+        private readonly IAspNetUser _user;
+
+        public ClientesController(IClienteRepository clienteRepository, IMediatorHandler mediator, IAspNetUser user)
+        {
+            _clienteRepository = clienteRepository;
+            _mediator = mediator;
+            _user = user;
+        }
+
+        [HttpGet("cliente/endereco")]
+        public async Task<IActionResult> ObterEndereco()
+        {
+            var endereco = await _clienteRepository.ObterEnderecoPorId(_user.ObterUserId());
+
+            return endereco == null ? NotFound() : CustomResponse(endereco);
+        }
+
+        [HttpPost("cliente/endereco")]
+        public async Task<IActionResult> AdicionarEndereco(AdicionarEnderecoCommand endereco)
+        {
+            endereco.ClienteId = _user.ObterUserId();
+            return CustomResponse(await _mediator.SendCommand(endereco));
+        }
+    }
+}
--- src/web/JSE.WebApp.MVC/Services/IComprasBffService.cs
+++ src/web/JSE.WebApp.MVC/Services/IComprasBffService.cs
@@ -5,11 +5,21 @@ namespace JSE.WebApp.MVC.Services
 {
     public interface IComprasBffService
     {
+        // Carrinho
         Task<CarrinhoViewModel> ObterCarrinho();
         Task<int> ObterQuantidadeCarrinho();
         Task<ResponseResult> AdicionarItemCarrinho(ItemCarrinhoViewModel produto);
         Task<ResponseResult> AtualizarItemCarrinho(Guid produtoId, ItemCarrinhoViewModel produto);
         Task<ResponseResult> RemoverItemCarrinho(Guid produtoId);
         Task<ResponseResult> AplicarVoucherCarrinho(string voucher);
+
+        // Pedido
+        Task<ResponseResult> FinalizarPedido(PedidoTransacaoViewModel pedidoTransacao);
+        Task<PedidoViewModel> ObterUltimoPedido();
+        Task<IEnumerable<PedidoViewModel>> ObterListaPorClienteId();
+        PedidoTransacaoViewModel MapearParaPedido(CarrinhoViewModel carrinho, EnderecoViewModel endereco);
+
+
+
     }
 }
