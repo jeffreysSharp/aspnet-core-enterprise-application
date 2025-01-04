9e300eece02e3df7d63a7ea22e1d5553bbeb612f
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-01-04 11:02:26 -0300

    #272 - Paginação no MVC - feature/sp8/#272

---
+++ src/web/JSE.WebApp.MVC/Models/IPagedList.cs
@@ -0,0 +1,12 @@
+﻿namespace JSE.WebApp.MVC.Models
+{
+    public interface IPagedList
+    {
+        public string ReferenceAction { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public string Query { get; set; }
+        public int TotalResults { get; set; }
+        public double TotalPages { get; }
+    }
+}
