da45dced05999ce32d478dbd7678311f4b4c3bf2
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-01-04 12:45:41 -0300

    #273 - ViewComponent de paginação /sp8 - feature/sp8/#273

---
+++ src/web/JSE.WebApp.MVC/Extensions/PaginacaoViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using JSE.WebApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JSE.WebApp.MVC.Extensions
+{
+    public class PaginacaoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(IPagedList modelopaginado)
+        {
+            return View(modelopaginado);
+        }
+    }
+}
