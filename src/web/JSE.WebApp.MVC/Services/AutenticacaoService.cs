6c314b0e4ef6907edc74628a14cabfbf2a54a954
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-01-15 15:55:25 -0300

    #280 - Refresh Token na App MVC - feature/sp8/#280

--- src/web/JSE.WebApp.MVC/Services/AutenticacaoService.cs
+++ src/web/JSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -1,20 +1,32 @@
 ﻿using JSE.Core.Communication;
+using JSE.WebAPI.Core.User;
 using JSE.WebApp.MVC.Extensions;
 using JSE.WebApp.MVC.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace JSE.WebApp.MVC.Services
 {
     public class AutenticacaoService : Service, IAutenticacaoService
     {
         private readonly HttpClient _httpClient;
+        private readonly IAuthenticationService _authenticationService;
+        private readonly IAspNetUser _user;
+
 
         public AutenticacaoService(HttpClient httpClient,
-                          IOptions<AppSettings> settings)
+                          IOptions<AppSettings> settings,
+                          IAuthenticationService authenticationService,
+                          IAspNetUser user)
         {
             httpClient.BaseAddress = new Uri(settings.Value.AutenticacaoUrl);
 
-            _httpClient = httpClient;            
+            _httpClient = httpClient;
+            _authenticationService = authenticationService;
+            _user = user;
         }
 
         public async Task<UsuarioRespostaLoginViewModel> Login(UsuarioLoginViewModel usuarioLogin)
@@ -49,6 +61,82 @@ namespace JSE.WebApp.MVC.Services
             }
 
             return await DeserializarObjetoResponse<UsuarioRespostaLoginViewModel>(response);
+        }
+
+        public async Task RealizarLogin(UsuarioRespostaLoginViewModel resposta)
+        {
+            var token = ObterTokenFormatado(resposta.AccessToken);
+
+            var claims = new List<Claim>();
+            claims.Add(new Claim("JWT", resposta.AccessToken));
+            claims.Add(new Claim("RefreshToken", resposta.RefreshToken));
+            claims.AddRange(token.Claims);
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties
+            {
+                ExpiresUtc = DateTimeOffset.UtcNow.AddHours(8),
+                IsPersistent = true
+            };
+
+            await _authenticationService.SignInAsync(
+                _user.ObterHttpContext(),
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+        }
+
+        public async Task Logout()
+        {
+            await _authenticationService.SignOutAsync(
+                _user.ObterHttpContext(),
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                null);
+        }
+
+        public static JwtSecurityToken ObterTokenFormatado(string jwtToken)
+        {
+            return new JwtSecurityTokenHandler().ReadToken(jwtToken) as JwtSecurityToken;
+        }
+
+        public async Task<UsuarioRespostaLoginViewModel> UtilizarRefreshToken(string refreshToken)
+        {
+            var refreshTokenContent = ObterConteudo(refreshToken);
+
+            var response = await _httpClient.PostAsync("/api/identidade/refresh-token", refreshTokenContent);
+
+            if (!TratarErrosResponse(response))
+            {
+                return new UsuarioRespostaLoginViewModel
+                {
+                    ResponseResult = await DeserializarObjetoResponse<ResponseResult>(response)
+                };
+            }
+
+            return await DeserializarObjetoResponse<UsuarioRespostaLoginViewModel>(response);
+        }
+
+        public bool TokenExpirado()
+        {
+            var jwt = _user.ObterUserToken();
+            if (jwt is null) return false;
+
+            var token = ObterTokenFormatado(jwt);
+            return token.ValidTo.ToLocalTime() < DateTime.Now;
+        }
+
+        public async Task<bool> RefreshTokenValido()
+        {
+            var resposta = await UtilizarRefreshToken(_user.ObterUserRefreshToken());
+
+            if (resposta.AccessToken != null && resposta.ResponseResult == null)
+            {
+                await RealizarLogin(resposta);
+                return true;
+            }
+
+            return false;
         }
     }
 }
